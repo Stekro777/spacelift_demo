6d1c404c87cd592240fa8ef467672a7bcd1f3279
Stekro777 <steffen.krogstad@gmail.com>
2024-01-10 10:43:56 +0100

    changed project root.

--- stack.tf
+++ stack.tf
@@ -3,8 +3,7 @@ resource "spacelift_stack" "tf-demo" {
   autodeploy        = true
   branch            = "main"
   description       = "Manage azure infrastructure"
-  name              = "Azure"
-  project_root      = "cluster"
+  name              = "azure_test"
   repository        = "tfdemo"
   terraform_version = "1.5.7"
 }
